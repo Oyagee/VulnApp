c3065c54848a4bb3c28bf325dee8f5e65c2de61b
Oyage <oyage@mail.ru>
2023-12-04 14:09:51 +0300

    IDOR

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 import sqlite3
 from markupsafe import escape
 
@@ -8,7 +8,6 @@ app = Flask(__name__, template_folder='templates')
 @app.route("/")
 def index():
     name = request.args.get("name", "")
-    name=escape(name)
     return f"<h1>Hello, {name}!</h1>"
 
 #SQLI
@@ -27,6 +26,31 @@ def sqli():
         return "<h1>Login successful.</h1>"
     else:
         return "<h1>Deny</h1>"
+
+
+
+
+
+############## IDOR
+
+
+users = [
+    {"id": 1, "name": "Роман", "email": "@mail.ru"},
+    {"id": 2, "name": "Алексей", "email": "@gmail.com"},
+]
+
+@app.route('/idor/')
+def idor():
+    return render_template('idor.html', users=users)
+
+
+@app.route('/user/<int:user_id>')
+def user_profile(user_id):
+    for user in users:
+        if user['id'] == user_id:
+            return render_template('profile.html', user=user)
+    return redirect('/')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
