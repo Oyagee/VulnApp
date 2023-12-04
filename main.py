e8acb5765333cd5f464ca3499328e3c10cfab189
Oyage <oyage@mail.ru>
2023-12-04 15:59:57 +0300

    Brute force

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def index():
     name = request.args.get("name", "")
     return f"<h1>Hello, {name}!</h1>"
 
-#SQLI
+#SQLI and BRUTE FORCE
 
 conn = sqlite3.connect('database.db', check_same_thread=False)
 cursor = conn.cursor()
@@ -24,10 +24,11 @@ def sqli():
     query = f"SELECT * FROM users WHERE username='{username}' AND password='{password}'"
     cursor.execute(query)
     result = cursor.fetchone()
+
     if result:
-        return "<h1>Login successful.</h1>"
+        return "<h1>Login successful.</h1>", 201
     else:
-        return "<h1>Deny</h1>"
+        return "<h1>Deny</h1>", 400
 
 
 
@@ -72,6 +73,23 @@ def read_file():
     except Exception as e:
         return str(e)
 
+
+
+### Brute Force
+
+@app.route('/login')
+def log():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.args.get("username", "")
+    password = request.args.get("password", "")
+
+    if username == 'admin' and password == 'password':
+        return "<h1>Login successful.</h1>"
+    else:
+        return "<h1>Deny.</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
